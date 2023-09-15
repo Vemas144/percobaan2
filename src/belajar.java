c2148480053ea3918950eb2dfb1f9833952f07fa
Vemas144 <144338919+Vemas144@users.noreply.github.com>
2023-09-15 13:22:03 +0700

    repo projek

---
+++ src/belajar.java
@@ -0,0 +1,23 @@
+public class belajar {
+    
+     public static void main(String[] args) {
+
+        // Deklarasi 
+        double Pi, r, luas;
+
+        // Deskripsi
+        r = 10.0;
+        Pi = 3.14;
+
+        //Proses
+        luas = pi * r * r;
+
+        //Output
+        System.out.println("Luas Lingkaran = ");
+        System.out.println(luas);
+        
+
+
+
+    }
+}
